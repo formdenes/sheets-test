19ddbd8c41c76337c6f31edd52c52eb7553a28f4
Formanek Dénes <formdenes@gmail.com>
2020-01-18 21:14:47 +0100

    a

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/sheets-test'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('./dist/sheets-test/index.html'));
+res.sendFile('./dist/sheets-test/index.html');
 });
 
 // Start the app by listening on the default Heroku port
