cdef9c179ebb0a2fad9b977bd637140150dad77e
Formanek Dénes <formdenes@gmail.com>
2020-01-18 20:35:52 +0100

    asdf

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sheets-test'));
+app.use(express.static('/dist/sheets-test'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/sheets-test/index.html'));
+res.sendFile(path.join('/dist/sheets-test/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
